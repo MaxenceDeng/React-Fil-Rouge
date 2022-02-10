2ea97237ab3474b76af62c67ca2320691f5f46d0
MaxenceDeng <97604913+MaxenceDeng@users.noreply.github.com>
2022-02-10 20:58:53 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Site from "./containers/Site/Site";
+import {BrowserRouter} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Site />
+    </BrowserRouter>
+  );
+}
+
+export default App;
